adb900e52f698c1502f02b915609d5c920f8ed93
rumi <user@LAPTOP-0IU79HM9git config --global user.name rumigit onfig --get-all user.namegit onfig --get-all user.nameGH-Rumigit onfig --get-all user.name-- get-all GH-Rumi!git config --global user.email user@LAPTOP-0IU79HM9>
2024-10-04 09:56:20 +0700

    Perubahan

--- Pemilihan2Percobaan111remake.java
+++ Pemilihan2Percobaan111remake.java
@@ -9,7 +9,7 @@ public class Pemilihan2Percobaan111remake {
         tahun = input11.nextInt();
 
         if (tahun % 4 == 0) {
-            if (tahun % 100 != 0) {
+            if (tahun % 100 == 0) {
                 System.out.println("Tahun Kabisat");
             } else {
                 if (tahun % 400 == 0) {
